93072717b328847ba94ed634bcb96fdf6f6875ea
Abdallah <eng.abdallahobaid@gmail.com>
2020-05-19 04:14:46 +0300

    done

--- challenges/array-shift.js
+++ challenges/array-shift.js
@@ -5,15 +5,15 @@ let arr = [4,8,15,23,123,321];
 let val = 16;
 let newArray = [];
 opj.insertShiftArray = function (array ,value ) {
-    for (let i = 0; i < Math.ceil(array.length/2); i++) {
-        newArray[i]= array[i];
-    }
-    newArray[Math.ceil(array.length/2)]=value;
-    for (let i = Math.ceil(array.length/2); i < array.length; i++) {
-      newArray[i+1]= array[i];
+  for (let i = 0; i < Math.ceil(array.length/2); i++) {
+    newArray[i]= array[i];
+  }
+  newArray[Math.ceil(array.length/2)]=value;
+  for (let i = Math.ceil(array.length/2); i < array.length; i++) {
+    newArray[i+1]= array[i];
   }
   return newArray;
-}
+};
 
 
 module.exports = opj;
