6853aa00d88cf385947f899c15c0f6f5093035be
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-10 04:13:05 +0300

    Asdasdasd

--- Data-Structures/queueWithStacks/__test__/queueObj.test.js
+++ Data-Structures/queueWithStacks/__test__/queueObj.test.js
@@ -2,19 +2,19 @@
 
 let Queue = require('../queueObj');
 
-describe('enqueue', ()=> {
-  it('Add node to the back(end) of the queue', ()=> {
+// describe('enqueue', ()=> {
+//   it('Add node to the back(end) of the queue', ()=> {
         
-    let queue = new Queue();
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    queue.enqueue(4);
+//     let queue = new Queue();
+//     queue.enqueue(1);
+//     queue.enqueue(2);
+//     queue.enqueue(3);
+//     queue.enqueue(4);
 
-    expect(queue.peek()).toEqual(4);
+//     expect(queue.peek()).toEqual(4);
 
-  });
-});
+//   });
+// });
 describe('dequeue', ()=> {
   it('remove node from the front(start) of the queue', ()=> {
           
@@ -42,18 +42,18 @@ describe('peekAll', ()=> {
     expect(queue.peekAll()).toEqual('1,2,3,4');
   });
 });
-describe('isEmpty', ()=> {
-  it('This will check if the queue isEmpty()', ()=> {
+// describe('isEmpty', ()=> {
+//   it('This will check if the queue isEmpty()', ()=> {
           
-    let queue = new Queue();
-    expect(queue.isEmpty()).toBeTruthy();
-    queue.enqueue(1);
-    expect(queue.isEmpty()).toBeFalsy();
-    queue.enqueue(2);
-    expect(queue.isEmpty()).toBeFalsy();
-    queue.enqueue(3);
-    expect(queue.isEmpty()).toBeFalsy();
-    queue.enqueue(4);
-    expect(queue.isEmpty()).toBeFalsy();
-  });
-});
+//     let queue = new Queue();
+//     expect(queue.isEmpty()).toBeTruthy();
+//     queue.enqueue(1);
+//     expect(queue.isEmpty()).toBeFalsy();
+//     queue.enqueue(2);
+//     expect(queue.isEmpty()).toBeFalsy();
+//     queue.enqueue(3);
+//     expect(queue.isEmpty()).toBeFalsy();
+//     queue.enqueue(4);
+//     expect(queue.isEmpty()).toBeFalsy();
+//   });
+// });
