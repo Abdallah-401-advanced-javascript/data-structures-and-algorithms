9b214d9c6286e17fe1ebdd7a3ac272ce0d2a277a
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-20 11:08:20 +0300

    done

--- Data-Structures/tree/tree.js
+++ Data-Structures/tree/tree.js
@@ -60,33 +60,33 @@ class BinaryTree {
   }
 }
    
-const Node = require('./node');
-let one = new Node(1);
-let two = new Node(2);
-let three = new Node(3);
-let four = new Node(4);
-let five = new Node(5);
-let six = new Node(6);
-let seven = new Node(7);
-let eight = new Node(8);
-let nine = new Node(9);
+// const Node = require('./node');
+// let one = new Node(1);
+// let two = new Node(2);
+// let three = new Node(3);
+// let four = new Node(4);
+// let five = new Node(5);
+// let six = new Node(6);
+// let seven = new Node(7);
+// let eight = new Node(8);
+// let nine = new Node(9);
 
-one.left = two;
-one.right = three;
-two.left = six;
-six.right = seven;
-seven.left = eight;
-seven.right = nine;
-three.left = four;
-three.right = five;
-let tree= new BinaryTree(one);
-// tree.inOrder(1);
-// tree.preOrder(2);
-// tree.preOrder(3);
-// tree.preOrder(4);
-// tree.preOrder(5);
-// tree.preOrder(6);
-console.log(tree);
+// one.left = two;
+// one.right = three;
+// two.left = six;
+// six.right = seven;
+// seven.left = eight;
+// seven.right = nine;
+// three.left = four;
+// three.right = five;
+// let tree= new BinaryTree(one);
+// // tree.inOrder(1);
+// // tree.preOrder(2);
+// // tree.preOrder(3);
+// // tree.preOrder(4);
+// // tree.preOrder(5);
+// // tree.preOrder(6);
+// console.log(tree);
 
 
-module.exports = BinaryTree;
+// module.exports = BinaryTree;
