4fecdf244be06b99568c89e95e3f87d650bd6817
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-28 08:45:35 +0300

    @@

---
+++ challenges/fizzBuzzTree/__test__/bt.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const BinaryTree = require('../fizz-buzz-tree');
+const Node = require('../node');
+
+describe('Binary Tree', ()=> {
+
+
+
+    it('preOrder()', ()=> {
+
+    });
+
+    it('inOrder()', ()=> {
+
+    });
+    
+    it('postOrder()', ()=> {
+
+    });
+
+});
